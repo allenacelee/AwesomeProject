3c8b281bfde09f23adaf7a9811e6879d7d687daa
Allen Lee <itman003@icloud.com>
2017-07-06 11:03:39 +0800

    测试动画

--- index.android.js
+++ index.android.js
@@ -7,47 +7,19 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Text
 } from 'react-native';
+import FadeInView from "./Modules/test/FadeInView";
 
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    render() {
+        return (
+            <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
+                <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Allen Lee</Text>
+            </FadeInView>
+        );
+    }
+}
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
